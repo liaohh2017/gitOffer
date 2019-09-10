7b2c5c60099461e8d1944f8c1a753f0e09eb1c1a
honghailiao <928050495@qq.com>
2019-09-10 21:15:09 +0800

    Update test.java
    20190910-2

--- src/gitOffer/test.java
+++ src/gitOffer/test.java
@@ -1,42 +1,33 @@
-package gitOffer;
-
-public class test {
-	/**
-	 * 数组交换
-	 * 
-	 * @param arr
-	 * @param i
-	 * @param j
-	 */
-	public static void swap(int[] arr, int i, int j) {
-		int tmp = arr[i];
-		arr[i] = arr[j];
-		arr[j] = tmp;
-	}
-
-	/**
-	 * 打印数组
-	 * 
-	 * @param arr
-	 */
-	public static void printArray(Integer[] arr) {
-		if (arr == null) {
-			return;
-		}
-		for (int i = 0; i < arr.length; i++) {
-			System.out.print(arr[i] + " ");
-		}
-		System.out.println();
-	}
-
-	public static void printArray(int[] arr) {
-		if (arr == null) {
-			return;
-		}
-		for (int i = 0; i < arr.length; i++) {
-			System.out.print(arr[i] + " ");
-		}
-		System.out.println();
-	}
-
+public class Solution {
+    /**
+     * @param nums: an array of integers
+     * @param s: an integer
+     * @return: an integer representing the minimum size of subarray
+     */
+    public int minimumSize(int[] nums, int s) {
+           if(nums == null || nums.length == 0)
+                     return -1;
+            int end =0;
+            int start = 0;
+            int sum = 0;
+            int res = Integer.MAX_VALUE;
+        while(end < nums.length){
+           sum +=nums[end];
+           if(sum >= s){
+              res = Math.min(res,end-start+1);
+              //当扫描到数组和大于s时，从第一个元素逐次缩小数组大小，
+                  //直到符合条件的最小子数组
+              while(sum >= s && start <= end){   
+                  res = Math.min(res,end-start+1);
+                  sum-=nums[start];
+                   start++;
+                    }
+                 }
+           end++;  //当前子数组的和还小于s,end往后移
+        }
+        if(res == Integer.MAX_VALUE ){   //需要考虑溢出问题
+           return -1;
+        }
+        return res;
+    }
 }
