1a2644ac21277b5764c14a0e74633cc02cd2fca6
liao <liaohh2017>
2019-09-10 21:09:55 +0800

    20190910-1

---
+++ src/gitOffer/test.java
@@ -0,0 +1,42 @@
+package gitOffer;
+
+public class test {
+	/**
+	 * 数组交换
+	 * 
+	 * @param arr
+	 * @param i
+	 * @param j
+	 */
+	public static void swap(int[] arr, int i, int j) {
+		int tmp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = tmp;
+	}
+
+	/**
+	 * 打印数组
+	 * 
+	 * @param arr
+	 */
+	public static void printArray(Integer[] arr) {
+		if (arr == null) {
+			return;
+		}
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+		System.out.println();
+	}
+
+	public static void printArray(int[] arr) {
+		if (arr == null) {
+			return;
+		}
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+		System.out.println();
+	}
+
+}
