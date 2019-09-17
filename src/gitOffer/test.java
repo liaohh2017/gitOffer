844f28ecaa62bc2670b98b66607847a3e90829c7
honghailiao <928050495@qq.com>
2019-09-17 19:48:13 +0800

    Update test.java

--- src/gitOffer/test.java
+++ src/gitOffer/test.java
@@ -1,29 +1,37 @@
-package gitOffer;
-//
-public class test {
-	public boolean isValidSudoku(char[][] board) {
-		// 记录某行，某位数字是否已经被摆放
-		boolean[][] row = new boolean[9][9];
-		// 记录某列，某位数字是否已经被摆放
-		boolean[][] col = new boolean[9][9];
-		// 记录某 3x3 宫格内，某位数字是否已经被摆放
-		boolean[][] block = new boolean[9][9];
+package test;
 
-		for (int i = 0; i < 9; i++) {
-			for (int j = 0; j < 9; j++) {
-				if (board[i][j] != '.') {
-					int num = board[i][j] - '1';
-					int blockIndex = i / 3 * 3 + j / 3;
-					if (row[i][num] || col[j][num] || block[blockIndex][num]) {
-						return false;
-					} else {
-						row[i][num] = true;
-						col[j][num] = true;
-						block[blockIndex][num] = true;
-					}
-				}
-			}
-		}
-		return true;
+
+
+public class p {
+
+
+    
+	private static final String p="p";
+	private static final String c="c";
+	public  p(){
+		System.out.println(p+"a");
 	}
+	static{
+		System.out.println(p+"b");
+	}
+	
+	{
+		System.out.println(p+"c");
+	}
+	static class c extends p{
+		public c(){
+			System.out.println(c+"a");
+		}
+		static{
+			System.out.println(c+"b");
+		}
+		
+		{
+			System.out.println(c+"c");
+		}
+	}
+	public static void main(String[] args) {
+		c c1=new c();
+	}
+
 }
