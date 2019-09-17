89b611f287df8e452be25a3107640467fb8e9798
honghailiao <928050495@qq.com>
2019-09-17 20:29:34 +0800

    Update test.java

--- src/gitOffer/test.java
+++ src/gitOffer/test.java
@@ -1,37 +1,73 @@
-package test;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
+public class Example {
+    static class Pair{
+        int first;
+        int second;
 
-public class p {
+        public Pair(int first, int second) {
+            this.first = first;
+            this.second = second;
+        }
 
+        public int getFirst() {
+            return first;
+        }
 
-    
-	private static final String p="p";
-	private static final String c="c";
-	public  p(){
-		System.out.println(p+"a");
-	}
-	static{
-		System.out.println(p+"b");
-	}
-	
-	{
-		System.out.println(p+"c");
-	}
-	static class c extends p{
-		public c(){
-			System.out.println(c+"a");
-		}
-		static{
-			System.out.println(c+"b");
-		}
-		
-		{
-			System.out.println(c+"c");
-		}
-	}
-	public static void main(String[] args) {
-		c c1=new c();
-	}
+        public void setFirst(int first) {
+            this.first = first;
+        }
 
+        public int getSecond() {
+            return second;
+        }
+
+        public void setSecond(int second) {
+            this.second = second;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int T = input.nextInt();
+        List<String> res = new ArrayList<>();
+
+        for (int i = 0; i < T; i++) {
+            int m = input.nextInt();
+            int n = input.nextInt();
+            List<Pair> list = new ArrayList<>();
+            for (int j = 0; j < n; j++) {
+                int f = input.nextInt();
+                int s = input.nextInt();
+                list.add(new Pair(Math.min(f,s),Math.max(f,s)));
+            }
+
+            int mid = 0;
+            String s = "IMPOSSIBLE";
+            for (int j = 0; j < n; j++) {
+                if(list.get(j).first == 1 && list.get(j).second == m) {
+                    s = "POSSIBLE";
+                    break;
+                } else if (list.get(j).first == 1 ) {
+                    if(list.get(j).second == mid){
+                        s = "POSSIBLE";
+                        break;
+                    } else {
+                        mid = list.get(j).second;
+                    }
+
+                } else if(list.get(j).second == m) {
+                    if(list.get(j).first == mid) {
+                        s = "POSSIBLE";
+                        break;
+                    }else
+                        mid = list.get(j).first;
+                }
+            }
+            res.add(s);
+        }
+        for(String x : res) System.out.println(x);
+    }
 }
