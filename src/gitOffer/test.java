78e0d87c08f9599feb23b663d4dc4d021d67258e
honghailiao <928050495@qq.com>
2019-09-22 16:39:01 +0800

    Update test.java

--- src/gitOffer/test.java
+++ src/gitOffer/test.java
@@ -1,47 +1,31 @@
-import java.math.BigInteger;
-import java.util.*;
-
-import java.util.*;
-
-import java.util.*;
-
-import java.util.*;
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String n1=sc.next();
-        String m1=sc.next();
-        BigInteger n=new BigInteger(n1);
-        BigInteger m=new BigInteger(m1);
-        String s=sc.next();
-        BigInteger k=new BigInteger(s);
-
-        if(n.multiply(m).compareTo(k)<=0){
-            System.out.println(0);
+public int jump(int[] nums) {
+        if (nums == null || nums.length <= 1) {
+            return 0;
         }
-        boolean flag=false;
-       int num=0;
-
-        while(!flag){
-            num+=1;
-            HashMap<Integer,Integer> map=new HashMap<>();
-            for (int i = 0; i <=num; i++) {
-                map.put(i,num-i);
-            }
-            for (int i = 0; i <=num/2 ; i++) {
-                BigInteger a=new BigInteger(String.valueOf(i));
-                BigInteger b=new BigInteger(String.valueOf(map.get(i)));
-                if(((n.subtract(a)).multiply(m.subtract(b))).compareTo(k)<=0||((n.subtract(b)).multiply(m.subtract(a)).compareTo(k)<=0)){
-                    System.out.println(num);
-                    flag=true;
-                    break;
-
-                }else {
-                    continue;
+        
+        return minJump(nums);
+    }
+    
+    private int minJump(int[] nums) {
+        int jump = 1;
+        int curIdx = 0;
+        int nextIdx = curIdx;
+        while (true) {
+            if(nums[curIdx] + curIdx < nums.length - 1) {
+                int maxDis = 0;
+                for (int j = 1; j <= nums[curIdx]; j++) {
+                    if (nums[curIdx + j] + curIdx + j > maxDis) {
+                        maxDis = nums[curIdx + j] + curIdx + j;
+                        nextIdx = curIdx + j;
+                    }
                 }
+                curIdx = nextIdx;
+                jump++;
+            } else {
+                break;
             }
-
         }
-
+        
+        return jump;
     }
 }
