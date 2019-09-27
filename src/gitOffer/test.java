4a4936f65eb2187eafa23b6a0110dcf77a1f99e0
honghailiao <928050495@qq.com>
2019-09-27 20:40:06 +0800

    Update test.java

--- src/gitOffer/test.java
+++ src/gitOffer/test.java
@@ -1,31 +1,30 @@
-public int jump(int[] nums) {
-        if (nums == null || nums.length <= 1) {
-            return 0;
-        }
-        
-        return minJump(nums);
-    }
-    
-    private int minJump(int[] nums) {
-        int jump = 1;
-        int curIdx = 0;
-        int nextIdx = curIdx;
-        while (true) {
-            if(nums[curIdx] + curIdx < nums.length - 1) {
-                int maxDis = 0;
-                for (int j = 1; j <= nums[curIdx]; j++) {
-                    if (nums[curIdx + j] + curIdx + j > maxDis) {
-                        maxDis = nums[curIdx + j] + curIdx + j;
-                        nextIdx = curIdx + j;
-                    }
-                }
-                curIdx = nextIdx;
-                jump++;
-            } else {
-                break;
-            }
-        }
-        
-        return jump;
-    }
-}
+ist<List<Integer>> res = new ArrayList<>();
+	public List<List<Integer>> permute(int[] nums) {
+		res.clear();
+		dfs(nums, 0);//
+		return res;
+	}
+	
+	public void dfs(int[] n, int start) {//start表示要被替换元素的位置
+		if( start >= n.length) {
+			List<Integer> list = new ArrayList<Integer>();
+			for(int i : n) {
+				list.add(i);
+			}
+			res.add(list);
+			return;
+		}
+		
+		for(int i = start; i< n.length; i++) {//i从start开始，如果从start+1开始的话，会把当前序列遗漏掉直接保存了下一个序列
+			int temp= n[i];
+			n[i] = n[start];
+			n[start] = temp;
+			dfs(n, start + 1);//递归下一个位置
+			//回到上一个状态
+			n[start] = n[i];
+			n[i] = temp;
+		}
+	}
+————————————————
+版权声明：本文为CSDN博主「qijingpei」的原创文章，遵循 CC 4.0 BY-SA 版权协议，转载请附上原文出处链接及本声明。
+原文链接：https://blog.csdn.net/qijingpei/article/details/83930870
